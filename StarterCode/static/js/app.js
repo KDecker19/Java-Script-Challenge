143b98fb43ede0fe8c7fd08c8e933604ecb5f30c
KDecker19 <kaleb.decker14@gmail.com>
2024-01-09 21:54:44 -0500

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,11 +1,10 @@
-// Read in samples.json using d3.json().
-
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 d3.json(url).then(function(data) {
     console.log(data);
-}
-);
+
+});
+
 
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
